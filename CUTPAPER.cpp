6fc7bf5a93ba51cd6b7818287b828781e3e356c6
rahul-shoundic <rahulshoundic@gmail.com>
2022-10-15 00:03:35 +0530

    added 1 problem

---
+++ CUTPAPER.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n,k;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>k;
+	    int x=n/k;
+	    cout<<x*x<<endl;
+	}
+	return 0;
+	
+}
