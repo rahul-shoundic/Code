e8e538af4d3fa04a5fcc575ab9e9dc0cd7e15979
rahul-shoundic <rahulshoundic@gmail.com>
2022-10-18 01:05:15 +0530

    added 1 problem

---
+++ DETSCORE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes herey
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    cout<<(x/10)*y<<endl;
+	}
+	return 0;
+}
