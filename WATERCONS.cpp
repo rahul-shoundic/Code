b586f9929372aa59f180d886386255cb31fe4fba
rahul-shoundic <rahulshoundic@gmail.com>
2022-11-14 22:47:10 +0530

    added 1 program

---
+++ WATERCONS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    (x>=2000)?cout<<"yes":cout<<"NO";
+	    cout<<endl;
+	}
+	return 0;
+}
