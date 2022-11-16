7a3e6fea1fcb39328e574ba5a789d876e8b82af4
rahul-shoundic <rahulshoundic@gmail.com>
2022-11-16 23:03:52 +0530

    added 1 problem

---
+++ PAR2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    (n%2==0)?cout<<"Yes":cout<<"No";
+	    cout<<endl;
+	}
+	return 0;
+}
